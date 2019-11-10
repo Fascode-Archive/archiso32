58ec4c7a561fae00a200d8b46d2ff0dea3e3f019
Erich Eckner <git@eckner.net>
2019-11-10 14:56:20 +0100

    configs/releng/build.sh: pull mirrorlist from archlinux32.org

--- configs/releng/build.sh
+++ configs/releng/build.sh
@@ -109,7 +109,7 @@ make_customize_airootfs() {
 
     cp ${script_path}/pacman.conf ${work_dir}/${arch}/airootfs/etc
 
-    curl -o ${work_dir}/${arch}/airootfs/etc/pacman.d/mirrorlist 'https://git.archlinux32.org/archlinux32/packages/raw/branch/master/core/pacman-mirrorlist/mirrorlist'
+    curl -o ${work_dir}/${arch}/airootfs/etc/pacman.d/mirrorlist 'https://archlinux32.org/mirrorlist/?country=all&protocol=http&protocol=https'
 
     lynx -dump -nolist 'https://wiki.archlinux.org/index.php/Installation_Guide?action=render' >> ${work_dir}/${arch}/airootfs/root/install.txt
 
